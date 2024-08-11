68b3c801a9298e97468ebe2f56e777327a341447
Michilis <120072772+Michilis@users.noreply.github.com>
2024-08-11 20:34:40 +0200

    Update validateNpub.js

--- src/utils/validateNpub.js
+++ src/utils/validateNpub.js
@@ -1,4 +1,4 @@
 module.exports = function validateNpub(npub) {
-  // Simple validation for Npub format. Adjust according to the actual format requirements.
+  // Simple validation for Npub format. Adjust according to actual format requirements.
   return /^npub[a-zA-Z0-9]{59}$/.test(npub);
 };
