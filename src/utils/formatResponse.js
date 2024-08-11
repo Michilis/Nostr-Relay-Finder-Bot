2f49c1f22cdb058df2461a5e5beaab891ee16002
Michilis <120072772+Michilis@users.noreply.github.com>
2024-08-11 20:35:00 +0200

    Update formatResponse.js

--- src/utils/formatResponse.js
+++ src/utils/formatResponse.js
@@ -1,7 +1,7 @@
-module.exports = function formatUserInfo(info) {
+module.exports = function formatResponse(info) {
   return `
 Npub: ${info.npub}
-Connected Relays: ${info.connectedRelays}
-Top Relays: ${info.topRelays.join(', ')}
+Connected Relays: ${info.connectedRelays || 'N/A'}
+Top Relays: ${info.topRelays ? info.topRelays.join(', ') : 'N/A'}
   `;
 };
